938ef4257cac57f589a8ffd53ec956f7aba11c49
siva <siva.thota@cognine.com>
2023-05-24 10:01:36 +0530

    firstcomit

---
+++ Decimal/Decimal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace DecToBinary
+{
+    class DeTBin
+    {
+        static  double n1 = 1.23;
+        static double n2 = 3.12;
+        int k1 = Convert.ToInt32(n1);
+        int k2 = Convert.ToInt32(n2);
+        
+        public static void Main(string[] args)
+        {
+            DeTBin obj = new DeTBin();
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(obj.k1);
+            Console.WriteLine(obj.k2);
+            Console.WriteLine(obj.Bconvertor(obj.k1));
+            Console.WriteLine(obj.Bconvertor(obj.k2));
+            Console.WriteLine(n1-obj.k1);
+            Console.WriteLine(n2-obj.k2);
+
+        }
+        public string Bconvertor(int n)
+        {
+            string s1="";
+            while(n!=0)
+            {
+
+                s1 += (char)n % 2;
+                n = n /2;
+
+            }
+            return s1;
+        }
+        
+    }
+}
---
+++ MaxfourDigit/MaxfourDigit/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Maxfour
+{
+    class Fourdigit
+    {
+
+        static string input = "3499994532324";
+        int[] array = new int[20];
+        int index = 0;
+        int max = 0;
+        public static void Main(string[] args)
+        {
+            Fourdigit obj_Digit = new Fourdigit();
+
+            if (input.Length < 4)
+            {
+                Console.WriteLine("not possible");
+            }
+
+            else if(input.Length==4)
+            {
+                Console.WriteLine(input);
+            }
+
+            else 
+            {
+                //input values dupping into the array by converting in to int;
+                while (obj_Digit.index != input.Length)
+                {
+                    int num = (int)input[obj_Digit.index]-48;
+
+                    obj_Digit.array[obj_Digit.index] = num;
+                    obj_Digit.index += 1;
+
+                }
+
+                // product of four adjecent digits
+                for (int j = 0; j < obj_Digit.array.Length - 3; j++)
+                {
+                    int fourdigit_product = obj_Digit.array[j] * obj_Digit.array[j + 1] *obj_Digit.array[j + 2] * obj_Digit.array[j + 3];
+
+                    //it checks which is max product
+                    if (fourdigit_product > obj_Digit.max)
+                    {
+                        obj_Digit.max = fourdigit_product;
+                    }
+                }
+                Console.WriteLine(obj_Digit.max);
+            }
+        }
+        
+    }
+}
